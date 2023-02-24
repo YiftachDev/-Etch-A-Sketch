6ed84c9cb4a00270778a20e4608029334197ac2c
YiftachDev <yiftach007@gmail.com>
2023-02-24 18:01:12 +0200

    Added event listner for mouse hovering

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ containerGrid.style.gridTemplateRows = `repeat(${gridSize}, 50px)`;
 containerGrid.style.gridTemplateColumns = `repeat(${gridSize}, 50px)`;
 
 
+for (let i = 0; i < gridSize ** 2; i++) {
+    const div = document.createElement('div');
+    containerGrid.appendChild(div);
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "black";
+    });
+}
