b2d28723923884a835c078b93d17a67e1ae14758
YiftachDev <yiftach007@gmail.com>
2023-02-24 18:20:33 +0200

    Added eraser functionality

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 let gridSize = 16;
+let color = "black";
+let eraserMode = false;
 const containerGrid = document.querySelector(".container");
 const styles = window.getComputedStyle(containerGrid);
 const gridTemplateRows = styles.getPropertyValue('grid-template-rows');
 const gridTemplateColumns = styles.getPropertyValue('grid-template-columns');
+const colorBtn = document.querySelector("#color-btn");
+const eraserBtn = document.querySelector("#eraser-btn");
+
 
 containerGrid.style.gridTemplateRows = `repeat(${gridSize}, 50px)`;
 containerGrid.style.gridTemplateColumns = `repeat(${gridSize}, 50px)`;
@@ -12,6 +17,18 @@ for (let i = 0; i < gridSize ** 2; i++) {
     const div = document.createElement('div');
     containerGrid.appendChild(div);
     div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "black";
+        if (!eraserMode) {
+            div.style.backgroundColor = color;
+        } else {
+            div.style.backgroundColor = "white";
+        }
     });
 }
+
+colorBtn.addEventListener("click", () => {
+    color = prompt("Color:");
+});
+
+eraserBtn.addEventListener("click", () => {
+    eraserMode = true;
+});
